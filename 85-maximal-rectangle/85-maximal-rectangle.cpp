943e55231f2b4ea34d35575da54e3976ba6aca2e
Sangam23 <72146976+sangam-coder@users.noreply.github.com>
2022-05-06 22:20:10 +0530

    Time: 96 ms (23.75%), Space: 20.7 MB (7.25%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+   int MAH(int*arr,int n)
+{  // next smaller to right side
+    stack<pair<int,int>>stk1;
+    vector<int>right;
+    for(int i=n-1;i>=0;i--)
+    {
+        while(!stk1.empty() and stk1.top().first>=arr[i])
+        {
+            stk1.pop();
+        }
+        if(stk1.empty())
+        {
+            right.push_back(n);
+        }
+        else
+            right.push_back(stk1.top().second);
+        
+        stk1.push({arr[i],i});
+    }
+    reverse(right.begin(),right.end());
+    // next smaller to left side of array
+    
+    stack<pair<int,int>>stk2;
+    vector<int>left;
+    for(int i=0;i<n;i++)
+    {
+        while(!stk2.empty() and stk2.top().first>=arr[i])
+        {
+            stk2.pop();
+        }
+        if(stk2.empty())
+        {
+            left.push_back(-1);
+        }
+        else
+        {
+            left.push_back(stk2.top().second);
+        }
+        stk2.push({arr[i],i});
+    }
+    
+    // make width array and area area;
+    vector<int>area;
+    for(int i=0;i<n;i++)
+    {
+        area.push_back((right[i]-left[i]-1)*arr[i]);
+    }
+    int no=*max_element(area.begin(),area.end());
+    return no;
+    
+}
+
+int maximalRectangle(vector<vector<char>>& matrix) 
+{   if(matrix.empty())
+    return 0;
+    int n=matrix[0].size();
+     
+    
+     int arr[200];
+     vector<int>check;
+     for(int i=0;i<n;i++)
+     {
+         arr[i]=0;
+     }
+    for(int i=0;i<matrix.size();i++)
+    {     int k=0;
+        for(int j=0;j<matrix[0].size();j++)
+        {
+            if(matrix[i][j]=='1')
+            {
+                arr[k]=arr[k]+1;
+                 k++;
+                
+            }
+            else
+            {
+                arr[k]=0;
+                k++;
+            }
+                
+       }
+       
+      int no=MAH(arr,n);
+       
+      check.push_back(no);
+    }
+    int ans=0;
+    ans=*max_element(check.begin(),check.end());
+    return ans;
+}
+};
