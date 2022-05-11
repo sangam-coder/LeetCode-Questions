c5ce1642c832639c8809f054022a38a8a5e68e75
Sangam23 <72146976+sangam-coder@users.noreply.github.com>
2022-05-11 17:03:33 +0530

    Time: 369 ms (5.03%), Space: 40.4 MB (11.39%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,20 +1,47 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
+        int n=nums.size();
+    vector<int>v;
+    int mine=nums[0];
+        v.push_back(nums[0]);
         
- stack <int> stack;
-        int second =  -2147483648;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums [i] < second)
-                return true;
-            while (stack.size() >0 && nums [i] > stack.top()){
-                second = stack.top ();
-                stack.pop();
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]<mine)
+            {
+                mine=nums[i];
+                v.push_back(mine);
             }
+            else 
+                v.push_back(mine);
+        }
+         stack<int>s;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(v[i]<nums[i])
+            {
+                 
+                while(s.size()&&s.top()<=v[i])
+                {
+                    s.pop();
+                }
                 
-            stack.push (nums [i]);
+               
+               if(s.size()&&nums[i]>s.top()) {
+                    return true;
+                }
+                
+                 
+                     s.push(nums[i]);
+                 
+            }
+            
+        }
+        for(int i=0;i<n;i++)
+        {
+        cout<<v[i]<<" ";
         }
         return false;
-        
     }
 };
