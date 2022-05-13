95c0c5dd6fd936b263d20ed82a43293f84ec4c0b
Sangam23 <72146976+sangam-coder@users.noreply.github.com>
2022-05-13 15:24:31 +0530

    Time: 11 ms (91.46%), Space: 17.6 MB (18.23%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        
+   
+  if(root==NULL) return NULL;
+    queue<Node*>q;
+        vector<Node*>v;
+        
+        q.push(root);
+        while(!q.empty())
+        {
+                while(!q.empty())
+                {
+                    Node* temp=q.front();
+                    q.pop();
+                    if(temp->left)
+                    {
+                        v.push_back(temp->left);
+                    }
+                    if(temp->right)
+                    {
+                        v.push_back(temp->right);
+                    }
+                    temp=NULL;
+                    
+                    
+                }
+                        if(!v.empty())
+                        {
+                         for(int i=0;i<v.size()-1;i++)
+                         {
+                             v[i]->next=v[i+1];
+                             q.push(v[i]);
+                         }
+                          q.push(v[v.size()-1]);
+                            v.clear();
+                            
+                            
+                        }
+//    /////////////////////////////////////////////////////////////////////////////////////////        
+        }
+        return root;
+    }
+};
