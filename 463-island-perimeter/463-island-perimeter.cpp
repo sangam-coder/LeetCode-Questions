6df1d32dae13c8c069a9de72e3bd3c755db14d35
Sangam23 <72146976+sangam-coder@users.noreply.github.com>
2022-05-18 00:39:08 +0530

    Time: 183 ms (27.75%), Space: 96.2 MB (85.42%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    int islandPerimeter(vector<vector<int>>& grid) {
-        
-      int count=0, repeat=0;
-        for(int i=0;i<grid.size();i++)
-        {
-            for(int j=0; j<grid[i].size();j++)
-                {
-                    if(grid[i][j]==1)
-                    {
-                        count ++;
-                        if(i!=0 && grid[i-1][j] == 1) repeat++;
-                        if(j!=0 && grid[i][j-1] == 1) repeat++;
-                    }
-                }
-        }
-        return 4*count-repeat*2;
     
-        
-        
-        
+    int DIR[5] = {0, 1, 0, -1, 0};
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size(), perimeter = 0;
+        for (int r = 0; r < row; ++r) {
+            for (int c = 0; c < col; ++c) {
+                if (grid[r][c] == 0) continue; // Skip water cell
+                perimeter += 4;
+                for (int i = 0; i < 4; ++i) {
+                    int nr = r + DIR[i], nc = c + DIR[i+1];
+                    if (nr < 0 || nr == row || nc < 0 || nc == col || grid[nr][nc] == 0) continue;
+                    perimeter -= 1;
+                }
+            }
+        }
+        return perimeter;
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 };
